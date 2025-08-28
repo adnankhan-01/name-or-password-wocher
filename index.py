592ae83e6f0dd26d9d2ce1df964c8cbd4f10b128
Adnan khan <kk437690322@gmail.com>
2025-08-28 12:28:18 +0500

    Create index.py
    first project of python name and password check the condection

---
+++ index.py
@@ -0,0 +1,7 @@
+
+name = input("emter your name ")
+password = input("emter your password")
+
+if name == "adnan" and password == "123":
+        print("werllcome to home noman")
+else :print("worring in wilid") 
